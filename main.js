879f9ef42be7619c977cc2ef5889ada815200fd8
zh99998 <zh99998@gmail.com>
2016-01-30 19:56:19 +0800

    fix and docker

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-/**
- * Created by zh99998 on 16/1/25.
- */
+'use strict';
+
 const http = require('http');
 const crypto = require('crypto');
 
-// Create an HTTP tunneling proxy
-
-var servers = [{
+let servers = [{
     "address": "122.0.65.73",
-    "port": 233
+    "port": 7911
 }];
 
-var pending = null;
+let pending = null;
 http.createServer((req, res) => {
     try {
-        var credentials = new Buffer(req.headers['authorization'].split(' ')[1], 'base64').toString().split(':');
-        var username = credentials[0];
-        var password = credentials[1];
+        let credentials = new Buffer(req.headers['authorization'].split(' ')[1], 'base64').toString().split(':');
+        let username = credentials[0];
+        let password = credentials[1];
         if (!username || !password) {
             throw 'auth';
         }
         //TODO: Auth
         console.log(username + ' requested match.');
+        res.username = username;
+        res.password = password;
     } catch (error) {
         res.statusCode = 403;
         res.end();
@@ -29,27 +28,43 @@ http.createServer((req, res) => {
     }
 
     if (pending) {
-        var server = servers[Math.floor(Math.random() * servers.length)];
-        var result = JSON.stringify({
-            "address": server.address,
-            "port": server.port,
-            "password": crypto.randomBytes(12).toString('base64')
-        });
+        let server = servers[Math.floor(Math.random() * servers.length)];
 
-        for (var client of [res, pending]) {
+        let room_id = crypto.randomBytes(9).toString('base64').slice(0, 11);
+        let options_buffer = new Buffer(6);
+        options_buffer.writeUInt8(4 << 4, 1);
+        let checksum = 0;
+        for (let i = 1; i < options_buffer.length; i++) {
+            checksum -= options_buffer.readUInt8(i)
+        }
+        options_buffer.writeUInt8(checksum & 0xFF, 0);
+
+        for (let client of [res, pending]) {
+            let buffer = new Buffer(6);
+            let secret = parseInt(client.password) % 65535 + 1;
+            for (let i = 0; i < options_buffer.length; i += 2) {
+                buffer.writeUInt16LE(options_buffer.readUInt16LE(i) ^ secret, i)
+            }
+            let password = buffer.toString('base64') + room_id;
+            let result = JSON.stringify({
+                "address": server.address,
+                "port": server.port,
+                "password": password
+            });
+
             client.writeHead(200, {'Content-Type': 'application/json', 'Cache-Control': 'no-cache'});
             client.end(result)
         }
         pending = null;
-        console.log('matched ' + result);
+        console.log('matched ' + room_id);
 
     } else {
         pending = res;
         res.on('close', ()=> {
-            console.log('connection closed.')
+            console.log('connection closed.');
             if (pending == res) {
                 pending = null;
             }
         })
     }
-}).listen(3001);
+}).listen(80);
